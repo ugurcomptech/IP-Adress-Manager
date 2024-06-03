9bb3fea7f0f40705855c585b226a9d7be81699f4
Uğur <sahinugurcan1233@gmail.com>
2024-06-03 15:43:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$allowed_ips_file = 'allowed_ips.txt';
+$allowed_ips = file($allowed_ips_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$visitor_ip = $_SERVER['REMOTE_ADDR'];
+
+if (!in_array($visitor_ip, $allowed_ips)) {
+    header('HTTP/1.1 403 Forbidden');
+    echo '403 Forbidden - Erişim izniniz yok.';
+    exit();
+}
+
+echo 'Hoş geldiniz, izin verilen IP adresinden giriş yaptınız!';
+?>
