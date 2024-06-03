ce790a2920a6abbdef763a3b73c9449307dcb28d
Uğur <sahinugurcan1233@gmail.com>
2024-06-03 23:35:05 +0300

    Update ip_ekle.php

--- ip_ekle.php
+++ ip_ekle.php
@@ -1,6 +1,6 @@
 <?php
 define('JSON_FILE', 'allowed_ips.json');
-define('ENCRYPTION_KEY', 'BuBirGizliAnahtar123'); 
+define('ENCRYPTION_KEY', 'BuBirGizliAnahtar123'); // Değiştirilmesi gereken güvenli bir anahtar
 
 function encryptData($data) {
     return openssl_encrypt($data, 'AES-256-CBC', ENCRYPTION_KEY, 0, ENCRYPTION_KEY);
