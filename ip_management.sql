16124610d923d6ac43131804f1016b3a50c680d5
Uğur <sahinugurcan1233@gmail.com>
2024-06-05 17:14:34 +0300

    Update ip_management.sql

--- ip_management.sql
+++ ip_management.sql
@@ -3,5 +3,8 @@ USE ip_management;
 
 CREATE TABLE ips (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    ip_address VARCHAR(45) NOT NULL
+    ip_address VARCHAR(45) NOT NULL,
+    expiry_date DATETIME,
+    creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expiry_seconds INT UNSIGNED NOT NULL DEFAULT 0
 );
